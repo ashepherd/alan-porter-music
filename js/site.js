8ec463e0230a139b0182a83a9ab3c3ab24991121
Adam Shepherd <2117576+ashepherd@users.noreply.github.com>
2021-11-07 13:04:40 -0500

    Create site.js

---
+++ js/site.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  document.querySelector('#navbarHeader').classList.toggle('is-active');
+  document.querySelector('#navbarHeaderBurger').classList.toggle('is-active');
+  
+});
