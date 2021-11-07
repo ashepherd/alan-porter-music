f5d1168ee954aacc5de1a62e7ed6626d775aabec
Adam Shepherd <2117576+ashepherd@users.noreply.github.com>
2021-11-07 13:40:54 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,6 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('#navbarHeaderBurger').addEventListener('click', () => {
-    document.querySelector('#navbarHeader').classList.toggle('is-active');
-    document.querySelector('#navbarHeaderBurger').classList.toggle('is-active');
-  });
+document.querySelector('#navbarHeaderBurger').addEventListener('click', () => {
+  document.querySelector('#navbarHeader').classList.toggle('is-active');
+  document.querySelector('#navbarHeaderBurger').classList.toggle('is-active');
 });
