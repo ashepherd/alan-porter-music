6eb6199573bc7b4ea33139cdf31090137fe2bc4f
Adam Shepherd <2117576+ashepherd@users.noreply.github.com>
2022-09-06 16:23:32 -0400

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,9 @@
-document.querySelector('#navbarHeaderBurger').addEventListener('click', () => {
-  document.querySelector('#navbarHeader').classList.toggle('is-active');
-  document.querySelector('#navbarHeaderBurger').classList.toggle('is-active');
-});
+function ready() {
+  document.querySelector('#navbarHeaderBurger').addEventListener('click', () => {
+    document.querySelector('#navbarHeader').classList.toggle('is-active');
+    document.querySelector('#navbarHeaderBurger').classList.toggle('is-active');
+  });
+  document.querySelector('div.powr-social-feed iframe').style.height = "1px";
+}
+
+document.addEventListener("DOMContentLoaded", ready);
