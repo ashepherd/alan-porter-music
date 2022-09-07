60335f20f7a0af32ec96cfc3ef986408bc19cd8e
Adam Shepherd <2117576+ashepherd@users.noreply.github.com>
2022-09-07 08:13:25 -0400

    Update site.js

--- js/site.js
+++ js/site.js
@@ -7,6 +7,17 @@ function ready() {
 
 document.addEventListener("DOMContentLoaded", ready);
 
+
+window.onorientationchange = function() { 
+        switch(window.orientation) { 
+            case 0:
+            case 90:
+            case -90: window.location.reload(); 
+            break; 
+        } 
+};
+
+/* FormBold throws a 500 error on optional fields that aren't filled out */
 function submitForm() {
   var DefaultValue = "N/A"; 
   if( document.contact.phone.value.length == 0 ) {
